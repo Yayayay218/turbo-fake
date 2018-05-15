448502891b7751758c7709c7fce3f6dd4bbcf7cf
Khan <khangds218@gmail.com>
2018-05-15 11:14:12 +0700

    X

--- src/config/AppSetting.js
+++ src/config/AppSetting.js
@@ -1,8 +1,8 @@
 const config = {
     // URL: 'http://162.243.163.4:3000/api',
     URL: 'https://videodl.net/api',
-    // SECRET_KEY: 'AIzaSyCOgq0Il0LAsFOSi_tXWQalUTzKri8miM4'
-    SECRET_KEY: 'AIzaSyB6j_A7UoCYpix5F9Dm4bShWWUuKU_aryQ'
+    SECRET_KEY: 'AIzaSyCOgq0Il0LAsFOSi_tXWQalUTzKri8miM4'
+    // SECRET_KEY: 'AIzaSyB6j_A7UoCYpix5F9Dm4bShWWUuKU_aryQ'
 };
 
 export default config
